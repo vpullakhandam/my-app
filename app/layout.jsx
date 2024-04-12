8f64950913e674eddd60f867a956246b3c1346d9
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2024-04-12 20:08:04 +0530

    commit

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+import "@styles/globals.css";
+
+
+export const metadata={
+    title:"Promptopia",
+    description:'Discover and Share AI prompts'
+}
+const layout = () => {
+  return <div>layout</div>;
+};
+
+export default layout;
